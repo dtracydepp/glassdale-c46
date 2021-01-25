6624a3efcf193b404b6d5a0efd9d2e4a39452df4
dtracydepp <tracydepp82@gmail.com>
2021-01-25 10:36:55 -0600

    added conviction provider and conviction select, added select element to add drop down that lists all the crimes

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
+import {ConvictionSelect} from "./convictions/ConvictionSelect.js"
+import { getConvictions, useConvictions } from "./convictions/ConvictionProvider.js"
 import { getCriminals, useCriminals } from "./criminals/CriminalDataProvider.js"
 import { CriminalList } from "./criminals/CriminalList.js"
 import { OfficerList } from "./officers/OfficerList.js"
 import {useOfficers, getOfficers} from "./officers/OfficerProvider.js"
+
 
 // useOfficers ()
 // getOfficers()
 // getCriminals ()
 // useCriminals ()
 
+useConvictions ()
+getConvictions ()
+
+ConvictionSelect ()
 CriminalList ()
 OfficerList ()
