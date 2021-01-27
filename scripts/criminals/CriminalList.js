1ad0477d8cc95943c4623c46db396bb58d859309
dtracydepp <tracydepp82@gmail.com>
2021-01-27 16:16:36 -0600

    implement event hub, added event listeners and and filtered the list of criminals.

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,28 +1,69 @@
-import {getCriminals, useCriminals} from "./CriminalDataProvider.js"
-import {Criminal} from "./Criminal.js"
+import { getCriminals, useCriminals } from "./CriminalDataProvider.js"
+import { Criminal } from "./Criminal.js"
+import {useConvictions} from "../convictions/ConvictionProvider.js"
+
 
 
 const criminalsContainer = document.querySelector(".criminalsContainer")
+const eventHub = document.querySelector(".container")
 
-export const CriminalList = () => {
-    getCriminals()
-        .then (() => {
-            const criminalArray = useCriminals()
+const renderToDom = (criminalCollection) => {
+    let criminalsHTMLRep = ""
 
-        let criminalsHTMLRep = ""
-            for (const criminal of criminalArray){
+    for (const criminal of criminalCollection) {
+        criminalsHTMLRep += Criminal(criminal)
+    }
 
-                criminalsHTMLRep += Criminal(criminal)
-
-                criminalsContainer.innerHTML = `
+    criminalsContainer.innerHTML = `
                 <h2>Glassdale Criminals<h2>
                 <section class="criminalList">
                 ${criminalsHTMLRep}
                 </section>   
                 `
-            }
-        // console.log("CriminalList",criminalsHTMLRep)
-
-        }) 
-      
 }
+// console.log("CriminalList",criminalsHTMLRep)
+
+export const CriminalList = () => {
+    getCriminals()
+        .then(() => {
+            const criminalsArray = useCriminals()
+            renderToDom(criminalsArray)
+
+        })
+
+}
+
+// Listen for the custom event dispatched in ConvictionSelect
+
+eventHub.addEventListener("crimeChosen", event => {
+    // use the property added to the event detail  in Custom Event --crimeThatWasChosen
+
+    if (event.detail.crimeThatWasChosen !== "0") {
+
+        // Get a copy of the array of convictions from the data provider
+
+        const convictionsArray = useConvictions()
+
+        // use the find method to get the first object in the convictions array that has the same id as the id of the chosen crime
+
+        const chosenConvictionObject = convictionsArray.find(convictionsObj => {
+            // console.log("check",convictionsObj)
+            return convictionsObj.id === parseInt(event.detail.crimeThatWasChosen)
+        })
+
+        // filter the criminals application state down to the ppl that committed the crime
+        // Get a copy of the array of criminals from the data provider
+
+       const criminalsArray = useCriminals ()
+
+        // filter the criminals data down to the people that committed the crime 
+
+        const filteredCriminalsArray = criminalsArray.filter(criminalObj => criminalObj.conviction === chosenConvictionObject.name)
+
+        // invoke the render() and pass the filtered collection as an argument
+        renderToDom(filteredCriminalsArray)
+
+    }
+
+
+})
