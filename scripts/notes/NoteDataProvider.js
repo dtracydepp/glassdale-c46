93e5e06941a8f937f0dc90399a7d87e1cde061be
dtracydepp <tracydepp82@gmail.com>
2021-01-29 15:00:49 -0600

    added note and note list comps, and event listener for show notenotes, notes are rendering to DOM when button clicked. added min styling.

--- scripts/notes/NoteDataProvider.js
+++ scripts/notes/NoteDataProvider.js
@@ -4,13 +4,17 @@ const eventHub = document.querySelector(".container")
 
 let notes = []
 
-const getNotes = () => {
+export const getNotes = () => {
     return fetch('http://localhost:8088/notes')
         .then(response => response.json())
         .then(parsedNotes => {
             notes = parsedNotes
         })
 
+}
+
+export const useNotes = () => {
+    return notes.slice()
 }
 
 export const saveNote = note => {
