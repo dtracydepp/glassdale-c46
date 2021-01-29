18e7e2dae95b1f9627e2dad30464b6650a18e144
dtracydepp <tracydepp82@gmail.com>
2021-01-29 15:11:30 -0600

    updated keys in note comp

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,8 +1,8 @@
 export const NoteHTMLConverter = (noteObject) => {
     return `
         <section class="note">
-            <div class="note__text">${ noteObject.text }</div>
-            <div class="note__suspect">Title: ${ noteObject.suspect }</div>
+            <div class="note__text">Note: ${ noteObject.text }</div>
+            <div class="note__suspect">Suspect: ${ noteObject.suspect }</div>
             <div class="note__author">Author: ${ noteObject.author }</div>
             <div class="note__date">Timestamp: ${ new Date(noteObject.date).toLocaleDateString('en-US')  }</div>
         </section>
