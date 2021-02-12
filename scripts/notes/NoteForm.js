cab728726305606b76a5685968a92649551d8416
dtracydepp <tracydepp82@gmail.com>
2021-02-12 15:33:11 -0600

    refactored NoteForm to add select element for criminals, added criminal id to note object, upated note list to render both notes and criminals

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,12 +1,22 @@
+// creates the note form and renders to the DOM, click event for the submit button, and submit will grab values from input and build a new object to post note on API.
+
 import {saveNote} from "./NoteDataProvider.js"
+import {getCriminals, useCriminals} from "../criminals/CriminalDataProvider.js"
 
 const contentTarget = document.querySelector(".noteFormContainer")
 const eventHub = document.querySelector(".container")
 
-const render = () =>{
+const render = (criminalsArray) =>{
     contentTarget.innerHTML = `
-    <label htmlFor="note-suspect">Suspect: </label>
-    <input type="text" id="note-suspect">
+    <select id="note-suspect" class="criminalSelect">
+    <option value="0">Please select a criminal...</option>
+    ${
+        criminalsArray.map(criminal=> {
+            return `<option value="${criminal.id}">${criminal.name}</option>`
+        }).join("")
+    }
+    </select>    
+    
     <label htmlFor="note-author">Author: </label>
     <input type="text" id="note-author">
     <label htmlFor="note-date">Date: </label>
@@ -19,21 +29,32 @@ const render = () =>{
 }
 
 export const NoteForm = () => {
-    render()
+    getCriminals() 
+        .then(() => {
+            const listOfCriminals = useCriminals()
+            render(listOfCriminals)
+        
+        
+    })
+    
+    
+    
+ 
+    
 }
 
 // Handle browser-generated click event in component
 eventHub.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "saveNote") {
 
-        const suspect = document.getElementById("note-suspect").value
+        const criminalId = parseInt(document.querySelector("#note-suspect").value)
         const author = document.getElementById("note-author").value
         const date = document.getElementById("note-date").value
         const text = document.getElementById("note-text").value
 
         // Make a new object representation of a note
         const newNote = {
-         "suspect": suspect,
+         "suspect": criminalId,
          "author": author,
          "date": date,
          "text": text
